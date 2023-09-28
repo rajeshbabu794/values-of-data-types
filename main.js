b03f640264f1c2e1c79a03ec5a2cd845b8399699
rajeshbabu794 <146095160+rajeshbabu794@users.noreply.github.com>
2023-09-28 23:21:11 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+function stringNumber(){
+
+
+let name="Rajesh";
+let number=87654321;
+    a=(name+" : "+number);
+     document.getElementById("rajesh1").textContent=a;
+
+    
+    
+}
+
+function BooleanTrueFalse(){
+     const isTodayThursday=true;
+     v=isTodayThursday;
+     document.getElementById("rajesh2").textContent=v;
+}
+
+ function change(){
+
+let myArray=["yesterday"]; 
+     k=myArray;
+     
+ document.getElementById("rajesh3").textContent=k;
+     
+ }
